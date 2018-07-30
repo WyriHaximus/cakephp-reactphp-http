da255bbd945bb71a61a2494a5c9f7e65d2b4fa23
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-30 18:48:30 +0200

    Missing Network namespace

--- src/Network/Session.php
+++ src/Network/Session.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace WyriHaximus\React\Cake\Http;
+namespace WyriHaximus\React\Cake\Http\Network;
 
 use Cake\Utility\Hash;
 use WyriHaximus\React\Http\Middleware\Session as MiddlewareSession;
